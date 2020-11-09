ffacb68b8b99e50d668f1fbd02fd19ddb1ba4ce4
Asad114-al <as3ad.94aa@gmail.com>
2020-11-09 22:06:58 +0100

    new

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+  const body = document.getElementById("bodycolor")
+
+
+  document.querySelector('#grauTaste').addEventListener('click', () => body.style.background="gray"  ) 
+  
+  document.querySelector('#grauTaste').addEventListener('click', () => body.style.color= "white" ) 
+  
+  
+  document.querySelector('#weissTaste').addEventListener('click', () => body.style.background="white")
+  
+  document.querySelector('#weissTaste').addEventListener('click', () =>body.style.color= "black")
+  
+  
+  document.querySelector('#blauTaste').addEventListener('click', () => body.style.background="blue" )
+  
+  document.querySelector('#blauTaste').addEventListener('click', () => body.style.color= "white")
+  
+  
+  document.querySelector('#gelbTaste').addEventListener('click', () => body.style.background="yellow") 
+  
+  document.querySelector('#gelbTaste').addEventListener('click', () => body.style.color= "black") 
